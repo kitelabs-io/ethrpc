6f2f7278bf2b4d9bdea205910303f697311419c1
batu4404 <batu4404@users.noreply.github.com>
2025-08-07 10:58:55 +0700

    chore: support ClientOptions for adapter

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -1,12 +1,13 @@
 package adapter
 
 import (
+	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/kitelabs-io/ethrpc/adapter/ethereum"
 )
 
-func New(chainID uint, url string) (EthClientAdapter, error) {
+func New(chainID uint, url string, options ...rpc.ClientOption) (EthClientAdapter, error) {
 	switch chainID {
 	default:
-		return ethereum.NewAdapter(url)
+		return ethereum.NewAdapter(url, options...)
 	}
 }
